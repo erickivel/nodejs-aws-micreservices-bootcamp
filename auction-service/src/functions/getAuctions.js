44de66a5eb96b71cdba640c9138674da485d0073
Eric Kivel <ericvkivel1@gmail.com>
2022-03-02 19:43:43 -0300

    getAuctions schema validation

--- auction-service/src/functions/getAuctions.js
+++ auction-service/src/functions/getAuctions.js
@@ -1,15 +1,29 @@
 import createError from 'http-errors';
+import validator from '@middy/validator';
 
+import getAuctionsSchema from '../lib/schemas/getAuctionsSchema';
 import commonMiddleware from '../lib/commonMiddleware';
 import { document } from '../utils/dynamodbClient';
 
 async function getAuctions(event, context) {
+  const { status } = event.queryStringParameters;
+
   let auctions;
 
+  const params = {
+    TableName: process.env.AUCTIONS_TABLE_NAME,
+    IndexName: 'statusAndEndDate',
+    KeyConditionExpression: '#status = :status',
+    ExpressionAttributeValues: {
+      ':status': status
+    },
+    ExpressionAttributeNames: {
+      '#status': 'status',
+    },
+  };
+
   try {
-    const result = await document.scan({
-      TableName: process.env.AUCTIONS_TABLE_NAME
-    }).promise();
+    const result = await document.query(params).promise();
 
     auctions = result.Items;
   } catch (error) {
@@ -23,4 +37,11 @@ async function getAuctions(event, context) {
   };
 }
 
-export const handler = commonMiddleware(getAuctions);
+export const handler = commonMiddleware(getAuctions)
+  .use(validator({
+    inputSchema: getAuctionsSchema,
+    ajvOptions: {
+      useDefaults: true,
+      strict: false,
+    },
+  }));
