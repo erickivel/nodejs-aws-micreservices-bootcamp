a88a5b451c1e4716b46384b7c3fca846e5c53094
Eric Kivel <ericvkivel1@gmail.com>
2022-03-05 21:07:06 -0300

    Notify Seller and Bidder

--- notification-service/src/handlers/sendMail.js
+++ notification-service/src/handlers/sendMail.js
@@ -3,19 +3,25 @@ import AWS from 'aws-sdk';
 const ses = new AWS.SES({ region: 'eu-west-1' });
 
 async function sendMail(event, context) {
+  const record = event.Records[0];
+  console.log('record processing ', record);
+
+  const email = JSON.parse(record.body);
+  const { subject, body, recipient } = email;
+
   const params = {
-    Source: 'YOUR_EMAIL',
+    Source: 'YOUR_AWS_VERIFIED_EMAIL',
     Destination: {
-      ToAddresses: ['YOUR_EMAIL'],
+      ToAddresses: [recipient],
     },
     Message: {
       Body: {
         Text: {
-          Data: 'Hello From Eric!'
+          Data: body,
         },
       },
       Subject: {
-        Data: 'Test Mail'
+        Data: subject,
       }
     },
   };
